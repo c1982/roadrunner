a13b8907bceb4001b86612d3a900b047cf24ce90
semihalev <semihalev@gmail.com>
2018-06-18 06:46:25 +0300

    close listener after start

--- unix_socket_factory.go
+++ unix_socket_factory.go
@@ -78,6 +78,8 @@ func (f *UnixSocketFactory) SpawnWorker(cmd *exec.Cmd) (w *Worker, err error) {
 	w.rl = rl
 	w.state.set(StateReady)
 
+	ls.Close()
+
 	return w, nil
 }
 
@@ -92,8 +94,6 @@ func (f *UnixSocketFactory) listen(ls net.Listener) {
 	if err != nil {
 		return
 	}
-
-	defer ls.Close()
 
 	rl := goridge.NewSocketRelay(conn)
 	if pid, err := fetchPID(rl); err == nil {
